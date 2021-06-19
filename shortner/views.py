f6bdae089dd96b6f741bd2394a938104044f8112
Athul Das <athuldas7777@gmail.com>
2021-06-19 18:40:39 +0530

    ...

---
+++ shortner/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import uuid
+from .models import Url
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=url, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    print(url_details.link)
+    print(url_details.uuid)
+    link = url_details.link.lstrip("https://")
+    return redirect('https://'+link)
